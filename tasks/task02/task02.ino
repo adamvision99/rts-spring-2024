67cf9df88e81194ea4f1f5db59152bfdec545642
Adam Astafyev <104013561+adamvision99@users.noreply.github.com>
2024-04-15 15:50:04 +0400

    Update task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,23 +1,55 @@
-#include <TaskManagerIO.h>
+#include <TaskScheduler.h>
 
-// Определение констант пинов и периодов в микросекундах
-const int pins[] = {3, 5, 6, 9, 10};
-const unsigned long periods[] = {10000, 1000, 500, 100, 50}; // Периоды для каждого пина
-const int numPins = sizeof(pins) / sizeof(pins[0]); // Количество пинов
-int states[numPins] = {LOW}; // Начальные состояния пинов, инициализированы как LOW
+// Инициализируем объект планировщика задач
+Scheduler scheduler;
+
+bool state1 = HIGH, state2 = HIGH, state3 = HIGH, state4 = HIGH, state5 = HIGH;
+
+// Создаем таски для каждого из импульсов
+Task task1(5000, TASK_FOREVER, [](){
+  digitalWrite(3, state1);
+  state1 = !state1;
+}, nullptr, true);
+
+Task task2(500, TASK_FOREVER, [](){
+  digitalWrite(5, state2);
+  state2 = !state2;
+}, nullptr, true);
+
+Task task3(250, TASK_FOREVER, [](){
+  digitalWrite(6, state3);
+  state3 = !state3;
+}, nullptr, true);
+
+Task task4(50, TASK_FOREVER, [](){
+  digitalWrite(9, state4);
+  state4 = !state4;
+}, nullptr, true);
+
+Task task5(25, TASK_FOREVER, [](){
+  digitalWrite(10, state5);
+  state5 = !state5;
+}, nullptr, true);
 
 void setup() {
-  // Настройка направления каждого пина на OUTPUT
-  for (int i = 0; i < numPins; i++) {
-    pinMode(pins[i], OUTPUT);
-    // Запланировать переключение каждого пина с соответствующим периодом
-    taskManager.scheduleFixedRate(periods[i], [i] {
-      states[i] = !states[i]; // Переключение состояния
-      digitalWrite(pins[i], states[i]); // Установить новое состояние пина
-    }, TIME_MICROS);
-  }
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  // Добавляем созданные таски в планировщик
+  scheduler.addTask(task1);
+  scheduler.addTask(task2);
+  scheduler.addTask(task3);
+  scheduler.addTask(task4);
+  scheduler.addTask(task5);
+
+  // Задаем интервал обновления планировщика (1 мс)
+  scheduler.startPeriodic(1);
 }
 
 void loop() {
-  taskManager.runLoop(); // Выполнение планировщика задач
+  // Запускаем планировщик задач
+  scheduler.execute();
 }
